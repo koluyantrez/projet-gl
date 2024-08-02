accec3bef0e1f67d1cb2d097a9d8171008b84721
Aiqyo <100310474+Aiqyo@users.noreply.github.com>
2024-08-02 13:19:00 +0200

    Create Request.vue

---
+++ frontend/src/popup/Request.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="popup">
+    <div class="inner">
+      <h3>Request Details</h3>
+      <p><strong>Name:</strong> {{ request.name }}</p>
+      <p><strong>First Name:</strong> {{ request.firstName }}</p>
+      <p><strong>Birth Date:</strong> {{ request.birthDate }}</p>
+      <p><strong>Address:</strong> {{ request.address }}</p>
+      <p><strong>City:</strong> {{ request.city }}</p>
+      <p><strong>Phone:</strong> {{ request.phone }}</p>
+      <p><strong>Filial:</strong> {{ request.filial }}</p>
+      <ItemAdd word="Accept" @click="acceptRequest"/>
+      <ItemAdd word="Reject" @click="rejectRequest"/>
+      <ItemAdd word="Close" @click="closePopup"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import ItemAdd from "@/elements/ItemAdd.vue";
+import axios from "axios";
+
+export default {
+  components: {ItemAdd},
+  props: {
+    request: Object,
+    show: Boolean
+  },
+  methods: {
+    acceptRequest() {
+      axios.post(`http://localhost:1937/secretariat/approve/${this.request.id}`)
+          .then(() => {
+            this.$emit('accept', this.request);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+    },
+    rejectRequest() {
+      axios.post(`http://localhost:1937/secretariat/reject/${this.request.id}`)
+          .then(() => {
+            this.$emit('reject', this.request);
+          })
+          .catch((error) => {
+            console.error(error);
+          });    },
+    closePopup() {
+      this.$emit('close');
+    }
+  }
+};
+</script>
+
+<style scoped>
+.popup {
+  position: fixed;
+  top: 0px;
+  bottom: 0px;
+  left: 0px;
+  right: 0px;
+  z-index: 99;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.inner {
+  position: relative;
+  background: rgb(255, 255, 255);
+  padding: 39px;
+  border-radius: 10%;
+  font-family: 'Arial', sans-serif;
+}
+
+
+</style>
