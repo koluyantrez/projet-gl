dc17cbce510291fb47cc06e44b1c0b04a54aed6c
Oussama Hakik <128186887+hakik2002@users.noreply.github.com>
2024-08-07 21:29:52 +0200

    connexion + ajouter secretariat

--- BACKEND/src/main/java/com/genieLogiciel/Umons/service/SecretariatService.java
+++ BACKEND/src/main/java/com/genieLogiciel/Umons/service/SecretariatService.java
@@ -2,8 +2,11 @@ package com.genieLogiciel.Umons.service;
 
 import com.genieLogiciel.Umons.auth.service.AbstractLoginService;
 import com.genieLogiciel.Umons.auth.service.AuthService;
+import com.genieLogiciel.Umons.auth.service.EmailDomain;
 import com.genieLogiciel.Umons.model.Secretariat;
 import com.genieLogiciel.Umons.repository.SecretariatRepository;
+import com.genieLogiciel.Umons.util.PersonnelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -14,7 +17,10 @@ import java.util.Optional;
 @Service
 public class SecretariatService extends AbstractLoginService {
     private SecretariatRepository secretariatRepository;
-    protected AuthService authService;
+
+    @Autowired
+    private UserService userService;
+    @Autowired private PersonnelMapper personnelMapper;
 
     public SecretariatService(SecretariatRepository secretariatRepository,AuthService authService) {
         super(authService);
@@ -22,13 +28,14 @@ public class SecretariatService extends AbstractLoginService {
     }
 
     public ResponseEntity<String> creer(Secretariat secretariat){
-        Secretariat secrBDD = this.secretariatRepository.findByMatricule(Math.toIntExact(secretariat.getMatricule()));
-        if(secrBDD != null){
-            return new ResponseEntity<>("Secretary member already exists.", HttpStatus.BAD_REQUEST);
-
+        ResponseEntity<String> response = userService.addUser(secretariat , EmailDomain.SECRETARIAT);
+        if (response.getStatusCode() == HttpStatus.OK){
+            secretariatRepository.save(secretariat);
+            personnelMapper.mapToPersonnel(secretariat);
+        }else if (response.getStatusCode() == HttpStatus.BAD_REQUEST){
+            return new ResponseEntity<>("Member already exist" , HttpStatus.OK);
         }
-        this.secretariatRepository.save(secretariat);
-        return new ResponseEntity<>("Secretary member added successfully", HttpStatus.OK);
+        return response;
     }
 
     public List<Secretariat> rechercher(){
@@ -55,6 +62,13 @@ public class SecretariatService extends AbstractLoginService {
      */
     @Override
     protected ResponseEntity<String> authenticate(Long matricule, String password) {
-        return null;
+        return secretariatRepository.findById(Math.toIntExact(matricule))
+                .map(secretariat -> {
+                    if (secretariat.getPassword().equals(password)) {
+                        return ResponseEntity.ok("Login successful");
+                    } else {
+                        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
+                    }
+                }).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not found"));
     }
 }
